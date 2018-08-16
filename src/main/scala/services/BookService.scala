23b9edf429c5261419175cb569720688787ffb71
dugol <danielugiraldo@gmail.com>
2018-08-16 17:10:17 -0500

    Prueba search

--- src/main/scala/services/BookService.scala
+++ src/main/scala/services/BookService.scala
@@ -5,7 +5,7 @@ import java.util.concurrent.Executors
 import cats.data.Reader
 import com.outworkers.phantom.ResultSet
 import entities.Book
-import repository.{RealBookRepository,BookRepository}
+import repository.{RealBookRepository, BookRepository}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -16,46 +16,44 @@ sealed trait BookServiceAlg {
 
   def updateBook(book: Book): Reader[BookRepository, Future[ResultSet]]
 
-  def searchBook(isbn: String): Reader[BookRepository,Future[Option[Book]]]
+  def searchBook(isbn: String): Reader[BookRepository, Future[Option[Book]]]
 }
 
 sealed trait BookService extends BookServiceAlg {
   private implicit val ecBook = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(20))
 
   override def createBook(book: Book): Reader[BookRepository, Future[ResultSet]] = Reader {
-    repository:BookRepository =>
-    RealBookRepository.searchBook(book.isbn)
-      .map(optionBook => optionBook.fold {
-        RealBookRepository.saveBook(book)
-      } { b => {
-        Future(throw new Exception("isbn existente"))
-      }
-      })
-      .flatten
-    //RealBookRepository.saveBook(book)
+    repository: BookRepository =>
+      repository.searchBook(book.isbn)
+        .map(optionBook => optionBook.fold {
+          RealBookRepository.saveBook(book)
+        } { b => {
+          Future(throw new Exception("isbn existente"))
+        }
+        })
+        .flatten
   }
 
   override def deleteBook(book: Book): Reader[BookRepository, Future[ResultSet]] = Reader {
-    repository:BookRepository=>
-    RealBookRepository.searchBook(book.isbn)
-      .map(ob => ob.fold {
-        throw new Exception("libro inexistente")
-      } { b => RealBookRepository.deleteBook(b) })
-      .flatten
-    //RealBookRepository.deleteBook(book)
+    repository: BookRepository =>
+      repository.searchBook(book.isbn)
+        .map(ob => ob.fold {
+          throw new Exception("libro inexistente")
+        } { b => RealBookRepository.deleteBook(b) })
+        .flatten
   }
 
-  override def updateBook(book: Book): Reader[BookRepository, Future[ResultSet]] = Reader{
-    repository:BookRepository=>
-    RealBookRepository.searchBook(book.isbn)
-      .map(ob => ob.fold {
-        throw new Exception("libro inexistente")
-      } { b => RealBookRepository.updateBook(b) }).flatten
+  override def updateBook(book: Book): Reader[BookRepository, Future[ResultSet]] = Reader {
+    repository: BookRepository =>
+      repository.searchBook(book.isbn)
+        .map(ob => ob.fold {
+          throw new Exception("libro inexistente")
+        } { b => RealBookRepository.updateBook(b) }).flatten
   }
 
-  override def searchBook(isbn: String): Reader[BookRepository,Future[Option[Book]]] = Reader{
-    repository:BookRepository=>
-    RealBookRepository.searchBook(isbn)
+  override def searchBook(isbn: String): Reader[BookRepository, Future[Option[Book]]] = Reader {
+    repository: BookRepository =>
+      repository.searchBook(isbn)
   }
 }
 
--- src/test/scala/BookTest.scala
+++ src/test/scala/BookTest.scala
@@ -5,22 +5,31 @@ import com.outworkers.phantom.connectors.KeySpace
 import entities.Book
 import org.apache.cassandra.db.Columns
 import org.scalatest.FunSuite
-import repository.BookRepository
+import repository.{BookRepository, TestBookRepository}
 import services.BookService
 
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, Future}
 
-class BookTest extends FunSuite{
+class BookTest extends FunSuite {
 
-  /*
+
   test("Consultar BD") {
     implicit val ecBook = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(20))
-    val res=BookService.searchBook("1234567")
+    val res = BookService.searchBook("1234567").run(TestBookRepository)
+    val res2 = Await.result(res, 10 seconds).get
+    assert(res2 == Book("test", "test", "test", "test", "test", "test", 0))
+  }
+}
+  /*
+  test("Crear book"){
+    implicit val ecBook = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(20))
+    val res=BookService.createBook(Book("","","","","","",0)).run(TestBookRepository)
     val res2=Await.result(res,10 seconds)
-    println(res2)
+    assert(res2==Future(ResultSet(ResultSet(null,null),null)))
   }
 
+  /*
   test("Service CreateBook"){
     implicit val ecBook = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(20))
     val resultSetC=BookService.createBook(Book("XXXXXXXX","XXXXXXXX","XXXXXXX","XXXXXX","XXXXXXXX","XXXXXXX",0))
